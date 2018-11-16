a5e35845a83595fdb6ea02e0e49d895137f35149
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-15 23:44:20 -0600

    Se agrego la clase BellmanFord con sus setter y getters en el archivo PruebaBELLMANFORD.java

--- src/PruebaBELLMANFORD.java
+++ src/PruebaBELLMANFORD.java
@@ -1,3 +1,66 @@
+//https://masfernandez.com/2013/04/16/algoritmo-bellman-ford-java/
+import java.io.*;
+import java.util.*;
+
+class BellmanFord {
+ 
+    private LinkedList<Aristas> aristas;
+    private float etiquetas[];
+    private int predecesor[];
+    private int numeroVertices, totalAristas, nodoOrigen;
+    private final int INFINITY = 999;
+ 
+    public LinkedList<Aristas> getAristas() {
+		return aristas;
+	}
+
+	public void setAristas(LinkedList<Aristas> aristas) {
+		this.aristas = aristas;
+	}
+
+	public float[] getEtiquetas() {
+		return etiquetas;
+	}
+
+	public void setEtiquetas(float[] etiquetas) {
+		this.etiquetas = etiquetas;
+	}
+
+	public int[] getPredecesor() {
+		return predecesor;
+	}
+
+	public void setPredecesor(int[] predecesor) {
+		this.predecesor = predecesor;
+	}
+
+	public int getNumeroVertices() {
+		return numeroVertices;
+	}
+
+	public void setNumeroVertices(int numeroVertices) {
+		this.numeroVertices = numeroVertices;
+	}
+
+	public int getTotalAristas() {
+		return totalAristas;
+	}
+
+	public void setTotalAristas(int totalAristas) {
+		this.totalAristas = totalAristas;
+	}
+
+	public int getNodoOrigen() {
+		return nodoOrigen;
+	}
+
+	public void setNodoOrigen(int nodoOrigen) {
+		this.nodoOrigen = nodoOrigen;
+	}
+
+	public int getINFINITY() {
+		return INFINITY;
+	}
 
 public class PruebaBELLMANFORD {
 
