83a32694c40045fc70e1ae677e8bef9b757da0e3
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-15 23:52:25 -0600

    commit
    Dentro de la clase BellmanFord se agrego una clase privada llamada Aristas

--- src/PruebaBELLMANFORD.java
+++ src/PruebaBELLMANFORD.java
@@ -61,7 +61,47 @@ class BellmanFord {
 	public int getINFINITY() {
 		return INFINITY;
 	}
-
+	private static class Aristas {
+		 
+        int origen, destino;
+        float coste;
+ 
+        public Aristas(int a, int b, float c) {
+            origen = a;
+            destino = b;
+            coste = c;
+        }
+ 
+        @Override
+        public String toString() {
+            return "Aristas{" + "origen=" + origen + ", destino=" + destino + ", coste=" + coste + '}';
+        }
+    }
+ 
+    public BellmanFord() throws IOException {
+        float item;
+        aristas = new LinkedList<Aristas>();
+        DataInputStream in = new DataInputStream(System.in);
+        System.out.print("Introduce numero de vertices ");
+        numeroVertices = Integer.parseInt(in.readLine());
+        System.out.println("Matriz de costes");
+        for (int i = 0; i < numeroVertices; i++) {
+            for (int j = 0; j < numeroVertices; j++) {
+                if (i != j) {
+                    System.out.println("Introduce coste del nodo " + (i + 1) + " al nodo " + (j + 1));
+                    item = Float.parseFloat(in.readLine());
+                    if (item != 0) {
+                        aristas.add(new Aristas(i, j, item));
+                    }
+                }
+            }
+        }
+        totalAristas = aristas.size();
+        etiquetas = new float[numeroVertices];
+        predecesor = new int[numeroVertices];
+        System.out.print("Introduce el vertice origen");
+        nodoOrigen = Integer.parseInt(in.readLine()) - 1;
+    }
 public class PruebaBELLMANFORD {
 
 	public static void main(String[] args) {
